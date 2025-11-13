d31bca73240e2ed7cf019cb2e74d36f8c1c3d2a1
Álvaro Olvera Fernández <aolvera@externas.jccm.es>
2025-11-13 20:05:47 +0100

    9.6a

--- web/themes/custom/forcontu_bartik/Gruntfile.js
+++ web/themes/custom/forcontu_bartik/Gruntfile.js
@@ -26,4 +26,5 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-sass');
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.registerTask('default',['watch']);
+  grunt.registerTask('dev',['sass','watch']);
 }
