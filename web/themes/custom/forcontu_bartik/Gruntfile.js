7d68f072405718d277bab3cb566ad59894f48411
Álvaro Olvera Fernández <aolvera@externas.jccm.es>
2025-11-13 20:04:38 +0100

    9.6a

---
+++ web/themes/custom/forcontu_bartik/Gruntfile.js
@@ -0,0 +1,29 @@
+module.exports = function(grunt) {
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    sass: {
+      dist: {
+        options: {
+          update: true,
+          sourcemap: 'auto'
+        },
+        files: {
+          'css/style.css' : 'scss/style.scss',
+          'css/layout/layout.css' : 'scss/layout/layout.scss',
+          'css/theme/theme.css' : 'scss/theme/theme.scss',
+          'css/component/component.css' : 'scss/component/component.scss',
+          'css/base/base.css' : 'scss/base/base.scss'
+        }
+      }
+    },
+    watch: {
+      css: {
+        files: '**/*.scss',
+        tasks: ['sass']
+      }
+    }
+  });
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.registerTask('default',['watch']);
+}
