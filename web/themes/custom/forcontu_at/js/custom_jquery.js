c6695ae17a488fe250d88b571683b6d00eda1370
Álvaro Olvera Fernández <aolvera@externas.jccm.es>
2025-11-17 00:09:06 +0100

    13.1b

--- web/themes/custom/forcontu_at/js/custom_jquery.js
+++ web/themes/custom/forcontu_at/js/custom_jquery.js
@@ -20,3 +20,27 @@
 
 })(jQuery);
 
+(function ($) {
+  'use strict';
+
+  $(document).ready(function () {
+
+    // select li items in the recent content block
+    var items = $('.block-plugin-id--views-block-content-recent-block-1 li');
+
+    // apply par and impar classes
+    items.each(function (index) {
+      if ((index + 1) % 2 === 0) {
+        $(this).addClass('par');
+      }
+      else {
+        $(this).addClass('impar');
+      }
+    });
+
+    // double font size of the first element
+    items.first().css('font-size', '2em');
+
+  });
+
+})(jQuery);
